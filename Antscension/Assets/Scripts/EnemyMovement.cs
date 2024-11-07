7d85890fb2cc7e59a8d85e1744846bfa28bb407e
danielBctr <danielbucataru69@gmail.com>
2024-11-07 14:24:02 +0200

    feat:add enemy, bounce object

---
+++ Antscension/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2 (-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+    }
+}
