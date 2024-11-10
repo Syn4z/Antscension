662383eaab7e3b87d799517fcca5062f5f3848bc
danielBctr <danielbucataru69@gmail.com>
2024-11-10 16:24:37 +0200

    fix: changed variables name

--- Antscension/Assets/Scripts/EnemyMovement.cs
+++ Antscension/Assets/Scripts/EnemyMovement.cs
@@ -4,16 +4,16 @@ using UnityEngine.InputSystem;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
-    Rigidbody2D myRigidBody;
+    Rigidbody2D enemyRigidBody;
 
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody2D>();
+        enemyRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        enemyRigidBody.velocity = new Vector2(moveSpeed, 0f);
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -24,6 +24,6 @@ public class EnemyMovement : MonoBehaviour
 
     void FlipEnemyFacing()
     {
-        transform.localScale = new Vector2 (-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+        transform.localScale = new Vector2 (-(Mathf.Sign(enemyRigidBody.velocity.x)), 1f);
     }
 }
