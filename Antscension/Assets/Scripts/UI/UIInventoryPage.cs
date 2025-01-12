7a0fa834baf50d1a00db4f7bdede85282943e13e
Grena30 <grenadinu@yahoo.com>
2025-01-12 15:48:21 +0200

    feat: add sample inventory system to all levels

--- Antscension/Assets/Scripts/UI/UIInventoryPage.cs
+++ Antscension/Assets/Scripts/UI/UIInventoryPage.cs
@@ -6,8 +6,19 @@ public class UIInventoryPage : MonoBehaviour
 {
     [SerializeField] private UIInventoryItem itemPrefab;
     [SerializeField] private RectTransform contentPanel;
+    [SerializeField] private UIInventoryDescription itemDescription;
 
     List <UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public Sprite image;
+    public int quantity;
+    public string title, description;
+
+    public void Awake()
+    {
+        Hide();
+        itemDescription.ResetDescription();
+    }
 
     public void InitializeInventoryUI(int inventorysize)
     {
@@ -16,12 +27,45 @@ public class UIInventoryPage : MonoBehaviour
             UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
             uiItem.transform.SetParent(contentPanel, false);
             listOfUIItems.Add(uiItem);
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
+    }
+
+    private void HandleShowItemActions(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleEndDrag(UIInventoryItem obj)
+    {
+        
+    }
+
+    private void HandleSwap(UIInventoryItem obj)
+    {
+        
+    }
+
+    private void HandleBeginDrag(UIInventoryItem inventoryItemUI)
+    {
+
+    }
+
+    private void HandleItemSelection(UIInventoryItem inventoryItemUI)
+    {
+        itemDescription.SetDescription(image, title, description);
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+
+        listOfUIItems[0].SetData(image, quantity);
     }
 
     public void Hide()
